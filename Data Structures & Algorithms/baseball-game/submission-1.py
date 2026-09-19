1926bf7ba869b021697b7893fe338ff7b914a63c
Andre Nguyen <35712233+andrenguyening@users.noreply.github.com>
2026-09-18 21:23:53 -0400

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for i in range(len(operations)):
+            if operations[i] == '+':
+                # Add last two scores
+                prev1 = record[-1]
+                prev2 = record[-2]
+                record.append(prev1 + prev2)
+            elif operations[i] == 'C':
+                # Remove last score
+                record.pop()
+            elif operations[i] == 'D':
+                # Add double previous score
+                record.append(int(record[-1] * 2))
+            else:
+                # Add to the record
+                record.append(int(operations[i]))
+        cnt = 0
+        for i in record:
+            cnt += i
+        return cnt
